b8e0d968c0291c6d83914e02dae88616c02f0f00
Total <39064108+TotalTakeover@users.noreply.github.com>
2023-12-20 16:42:28 -0700

    Removal of discontinued versions
    - Removed non trim version
    - Removed non armor version

--- NoCustomArmor/scripts/DamageSound.lua
+++
@@ -1,6 +0,0 @@
--- Hit sound
-function events.TICK()
-	if player:getNbt()["HurtTime"] == 10 then
-		sounds:playSound("minecraft:entity.salmon.hurt", player:getPos(), 0.6, math.random()+0.5)
-	end
-end
