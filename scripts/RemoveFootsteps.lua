05155629f8f719070f4a15268f4d7d262403cef6
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-03-27 23:44:38 -0700

    Update RemoveFootsteps.lua
    - Made to be more concise, directly designed by the Figura community.
    - Added comments

--- scripts/RemoveFootsteps.lua
+++ scripts/RemoveFootsteps.lua
@@ -2,11 +2,18 @@
 local merlingParts = require("lib.GroupIndex")(models.models.Merling)
 local average      = require("lib.Average")
 
--- Remove Footsteps
-function events.on_play_sound(id, pos, _, _, _, path)
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
 	
-	if average(merlingParts.Tail1:getScale():unpack()) >= 0.75 then
-		return player:isLoaded() and (pos - player:getPos()):lengthSquared() < 1 and id:find("step") and path == "PLAYERS"
-	end
+	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
+	if not path then return end
+	
+	-- Don't do anything if the user isn't loaded
+	if not player:isLoaded() then return end
+	
+	-- Make sure the sound is (most likely) played by the user
+	if (player:getPos() - pos):length() > 0.05 then return end
+	
+	-- If sound contains ".step", and the user's merling is above the 0.75 sounds threshold, stop the actual step sound
+	if id:find(".step") and average(merlingParts.Tail1:getScale():unpack()) >= 0.75 then return true end
 	
 end
