51aa073f6ac4047084f73b8412a6fcc2c58efbae
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-06-26 13:11:47 -0700

    Damage Update
    - Change damage sound to completely override default damage sound

--- scripts/DamageSound.lua
+++ scripts/DamageSound.lua
@@ -1,8 +1,17 @@
-function events.TICK()
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
 	
-	-- Hit sound
-	if player:getNbt()["HurtTime"] == 10 then
-		sounds:playSound("entity.salmon.hurt", player:getPos(), 0.6, math.random()+0.5)
+	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
+	if not path then return end
+	
+	-- Don't do anything if the user isn't loaded
+	if not player:isLoaded() then return end
+	
+	-- Make sure the sound is (most likely) played by the user
+	if (player:getPos() - pos):length() > 0.05 then return end
+	
+	-- If sound contains ".hurt", stop the actual damage sound, and replace it
+	if id:find(".hurt") then
+		return sounds:playSound("entity.salmon.hurt", pos, 0.6, math.random()+0.5)
 	end
 	
 end
