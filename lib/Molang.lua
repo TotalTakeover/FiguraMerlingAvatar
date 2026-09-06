ebaf4a86fb67a66dda6a4a0c712051ecce3d0dcc
Total <39064108+TotalTakeover@users.noreply.github.com>
2026-09-05 17:45:19 -0700

    Molang update
    - `Math` now uses direct variable functions rather than searching through the `math` class
    - Renamed `q` to `query`
      - Copied  `query` to `q`
    - Instead of checking the key and doing a function, a table lookup is done instead with the key containing the function that would be preformed.

--- lib/Molang.lua
+++ lib/Molang.lua
@@ -1,24 +1,46 @@
+-- Math functions
+local _sin, _cos, _rad = math.sin, math.cos, math.rad
+
 -- Molang to Lua conversions
 Math = {
-	sin = function(a)
-		return math.sin(math.rad(a))
+	---@param deg number
+	sin = function(deg)
+		return _sin(_rad(deg))
 	end,
-	cos = function(a)
-		return math.cos(math.rad(a))
+	---@param deg number
+	cos = function(deg)
+		return _cos(_rad(deg))
 	end
 }
 
--- Allow q.anim_time to be interpreted as anim:getTime()
-q = {}
-local anim
-setmetatable(q, {
-	__index = function(t, i)
-		if i == "anim_time" then
-			return anim:getTime()
+-- A table of functions that will use an animation to return their values, based on the key provided.
+---@type function[]
+local animKeys = {
+	-- Gets an animation's time with `anim_time`.
+	---@param self Animation
+	anim_time = function(self) return self:getTime() end
+}
+
+-- The animation that is playing at the time of key checking.
+---@type Animation
+local currAnim
+
+-- The global variable Animations will use, matching Molang's `Query` or `q`.
+query = setmetatable({},
+	{
+		-- Determines the current animation that is playing when a key is accessed.
+		---@param anim Animation
+		__call = function(_, _, anim)
+			currAnim = anim
+			return 0
+		end,
+		-- Determines the value of the key.
+		---@param key string
+		__index = function(_, key)
+			return animKeys[key](currAnim)
 		end
-	end,
-		__call = function(t, _, a)
-		anim = a
-		return 0
-	end 
-})
+	}
+)
+
+-- Copy to alias
+q = query
